dc11d1e99ade693ef4c1df44b82f4fc470ce2e10
Jmhcoder6 <120882029+Jmhcoder6@users.noreply.github.com>
2023-03-17 23:01:25 +0800

    Update index.py

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ login = ''
 def index(id=None):
     global name, login
     if request.method == 'POST':
-        if request.form.get('account') == 'admin' and request.form.get('pwd') == 'admin':
+        if request.form.get('account') == 'jmhwoaini' and request.form.get('pwd') == 'jmhwoaini':
             login = 'success'
             name = 'admin'
             return render_template('1.html', name=name, login=login)
